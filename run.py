edb785915b204a8e2a74de25fe490e455adf54b4
Laura O Donnell <lauraodonnell90@hotmail.com>
2024-08-21 17:42:33 +0000

    Initial commit to set up and add welcome message

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+name = input("Please type your name")
+
+print("Good Luck, lets get started!", name)
+
