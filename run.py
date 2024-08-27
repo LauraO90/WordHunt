935215a0928f76a22876eee2d1bc0d0c2b70cbef
Laura O Donnell <lauraodonnell90@hotmail.com>
2024-08-27 15:38:21 +0000

    Update following validator check

--- run.py
+++ run.py
@@ -4,12 +4,14 @@ from animals import animal_words
 from plants import plant_words
 from planets import planet_words
 
+
 def enter_wordhunt():
     print("Please type your name")
     name = input()
 
     print("Good Luck, lets get started", name)
     play_wordhunt()
+
 
 def play_wordhunt():
     words = get_category_words()
@@ -19,16 +21,16 @@ def play_wordhunt():
     number_guesses = number_letters(active_word) + 1
     number_attempts = 0
 
-    print(f"Let's hunt - you have {number_guesses} attempts to guess the word!")
+    print(f"Let's hunt - you have {number_guesses} attempts to guess!")
     current_status = ''
     for x in range(len(active_word)):
         current_status += ' _'
-        
+
     print(current_status.strip())
 
     while number_attempts < number_guesses:
         number_attempts += 1
-      
+
         guess = get_user_guess(guesses)
         guesses.append(guess)
 
@@ -37,23 +39,23 @@ def play_wordhunt():
         for letter in active_word:
             if letter.lower() in guesses:
                 current_status += letter
-            else: 
+            else:
                 current_status += ' _'
-        
-        print(current_status.strip())
+    print(current_status.strip())
 
-        if current_status == active_word:
-            print("Well done - you found the word!")
-            restart_or_exit()
-    
+    if current_status == active_word:
+        print("Well done - you found the word!")
+        restart_or_exit()
+
     print("Hard luck, you are out of guesses")
     print("The word was", active_word)
     restart_or_exit()
 
+
 def number_letters(word):
     return len(set(word.lower()))
 
-    
+
 def get_user_guess(existing_guesses):
     guess = input()
     if guess in existing_guesses:
@@ -63,7 +65,7 @@ def get_user_guess(existing_guesses):
     else:
         return guess
 
-    
+
 def get_category_words():
     print("\nPlease choose a category:\n")
 
@@ -81,9 +83,10 @@ def get_category_words():
     elif category_choice == "c":
         print("\nYou chose PLANETS\n")
         return planet_words
-    else: 
+    else:
         print("Please choose a valid category")
         return get_category_words()
+
 
 def restart_or_exit():
     print("Would you like to play again?")
@@ -93,6 +96,7 @@ def restart_or_exit():
         play_wordhunt()
     else:
         exit(1)
+
 
 if __name__ == '__main__':
     enter_wordhunt()
