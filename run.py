6d67e0ebb326a6cc0665809fe3f6c69cd53161da
Laura O Donnell <lauraodonnell90@hotmail.com>
2024-08-29 20:46:57 +0000

    Add code to clear terminal during game

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 import random
+import os
 
 from animals import animal_words
 from plants import plant_words
 from planets import planet_words
 
 
+def welcome():
+    print("Welcome to Wordhunt!")
+    print("How to play?")
+    print("You will have a choice of THREE categories.")
+    print("Once you have chosen, you will be presented with a word to guess.")
+    print("Please ensure your guesses are single letters only.")
+    print("Happy hunting!")
+
+
 def enter_wordhunt():
+    welcome()
     print("Please type your name")
     name = get_username()
 
@@ -29,36 +40,63 @@ def play_wordhunt():
     guesses = []
     number_guesses = number_letters(active_word) + 2
     number_attempts = 0
+    active_word_length = len(active_word)
 
-    print(f"Let's hunt - you have {number_guesses} attempts to guess!")
+    print(f"""Let's hunt - you have {number_guesses}
+    attempts to guess the word:""")
+    print(f"Your word has {active_word_length} letters:")
+
     current_status = ''
-    for x in range(len(active_word)):
+    for x in range(active_word_length):
         current_status += ' _'
 
     print(current_status.strip())
 
     while number_attempts < number_guesses:
+        current_status = get_current_status(active_word, guesses)
+
+        if number_attempts > 0:
+            clear_terminal()
+            print("So far you have found:")
+            print(current_status)
+            if (number_attempts is number_guesses - 1):
+                print(f"You have 1 guess left")
+            else:
+                print(f"""You have {number_guesses - number_attempts}
+                guesses left""")
+
+            print("Choose your next letter")
+        else:
+            print("Choose your first letter")
+
         number_attempts += 1
 
         guess = get_user_guess(guesses)
         guesses.append(guess)
+        current_status = get_current_status(active_word, guesses)
 
-        current_status = ''
+        if current_status == active_word:
+            clear_terminal()
+            print("Well done - you found the word!")
+            print(current_status)
+            restart_or_exit()
 
-        for letter in active_word:
-            if letter.lower() in guesses:
-                current_status += letter
-            else:
-                current_status += ' _'
-        print(current_status.strip())
-
-    if current_status == active_word:
-        print("Well done - you found the word!")
-        restart_or_exit()
-
+    clear_terminal()
     print("Hard luck, you are out of guesses")
+    print(current_status)
     print("The word was", active_word)
     restart_or_exit()
+
+
+def get_current_status(active_word, guesses):
+    current_status = ''
+
+    for letter in active_word:
+        if letter.lower() in guesses:
+            current_status += letter
+        else:
+            current_status += ' _'
+    return current_status.strip()
 
 
 def number_letters(word):
@@ -79,7 +117,7 @@ def get_user_guess(existing_guesses):
         return get_user_guess(existing_guesses)
     else:
         return guess
-    
+
 
 def get_category_words():
     print("\nPlease choose a category:\n")
@@ -90,12 +128,15 @@ def get_category_words():
     category_choice = input()
 
     if category_choice == "a":
+        clear_terminal()
         print("\nYou chose PLANTS\n")
         return plant_words
     elif category_choice == "b":
+        clear_terminal()
         print("\nYou chose ANIMALS\n")
         return animal_words
     elif category_choice == "c":
+        clear_terminal()
         print("\nYou chose PLANETS\n")
         return planet_words
     else:
@@ -111,6 +152,10 @@ def restart_or_exit():
         play_wordhunt()
     else:
         exit(1)
+
+
+def clear_terminal():
+    os.system('cls || clear')
 
 
 if __name__ == '__main__':
